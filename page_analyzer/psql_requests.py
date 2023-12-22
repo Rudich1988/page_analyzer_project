83bddd488a25330ecaa22ca47900df6368618116
Игорь Рудич <rudi4.clarinet@mail.ru>
2023-12-22 21:26:26 +0300

    correct readme

--- page_analyzer/psql_requests.py
+++ page_analyzer/psql_requests.py
@@ -6,12 +6,15 @@ SHOW_ALL_WEBSITES = ("SELECT urls.id, urls.name, url_checks.status_code, "
 
 
 def get_url_data_request_with_id(id):
+    
     return (f"SELECT urls.id, urls.name, urls.created_at, "
             f"url_checks.id, url_checks.status_code, "
             f"url_checks.h1, url_checks.title, url_checks.description, "
             f"url_checks.created_at FROM urls "
             f"LEFT JOIN url_checks ON urls.id = url_checks.url_id "
             f"WHERE urls.id = {id} ORDER BY url_checks.id DESC;")
+    
+    #return (f"SELECT * FROM url_checks WHERE url_id = {id} ORDER BY id DESC")
 
 
 def get_url_data_request_with_url(website_url):
@@ -24,6 +27,9 @@ def get_url_data_request_with_url(website_url):
             f"ON urls.id = url_checks.url_id WHERE "
             f"urls.name = '{website_url}' "
             f"ORDER BY url_checks.id DESC;")
+    
+
+    #return (f"SELECT * FROM urls_checks WHERE ")
 
 
 def get_all_urls_with_id(id):
