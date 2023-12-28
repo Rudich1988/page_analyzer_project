c425b4e63e4deff30fa8356d74759debb5dfecf5
Игорь Рудич <rudi4.clarinet@mail.ru>
2023-12-28 03:10:35 +0300

    check2 cor 9

--- page_analyzer/url_utils.py
+++ page_analyzer/url_utils.py
@@ -1,4 +1,12 @@
 from urllib.parse import urlparse
+import validators
+
+
+LINE_LENGTH = 255
+
+
+def is_url_valid(website_url):
+    return validators.url(website_url) and len(website_url) <= LINE_LENGTH
 
 
 def normalize_url(website_name):
